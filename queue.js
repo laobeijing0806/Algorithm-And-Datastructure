71f9a3a9815ce3db0b809e217eb4b28520630ef1
liubojun <1570680014@qq.com>
2017-08-08 22:01:26 +0800

    update queue and stack

--- queue.js
+++ queue.js
@@ -1,13 +1,83 @@
+// 用类来实现队列
 function Queue () {
 
   var items = []
 
+  // 向队列尾部添加元素
   this.enqueue = function (element) {
     items.push(element)
   }
 
+  // 删除队列头部的元素并返回删除元素
   this.dequeue = function () {
     return items.shift()
+  }
+
+  // 返回队列头部的元素
+  this.front = function () {
+    return items[0]
+  }
+
+  // 队列是否为空
+  this.isEmpty = function () {
+    return items.length === 0
+  }
+
+  // 返回队列里元素的个数
+  this.size = function () {
+    return items.length
+  }
+
+  // 打印队列
+  this.print = function () {
+    console.log(items.toString())
+  }
+}
+
+// 一些对队列的操作
+// var queue = new Queue()
+// console.log(queue.isEmpty())
+// queue.enqueue('John')
+// queue.enqueue('Jack')
+// queue.enqueue('Camila')
+// queue.print()
+// console.log(queue.size())
+// console.log(queue.isEmpty())
+// queue.dequeue()
+// queue.dequeue()
+// queue.print()
+
+// 优先队列的定义
+function PriorityQueue () {
+  var items = []
+
+  function QueueElement (element, priority) {
+    this.element = element
+    this.priority = priority
+  }
+
+  // 给元素设置优先值，再按优先值插入队列
+  this.enqueue = function (element, priority) {
+    var queueElement = new QueueElement(element, priority)
+    if (this.isEmpty()) {
+      items.push(queueElement)
+    } else {
+      var added = false
+      for (var i = 0; i < items.length; i++) {
+        if (queueElement.priority < items[i].priority) {
+          items.splice(i, 0, queueElement)
+          added = true
+          break
+        }
+      }
+      if (!added) {
+        items.push(queueElement)
+      }
+    }
+  }
+
+  this.dequeue = function () {
+    items.shift()
   }
 
   this.front = function () {
@@ -23,70 +93,11 @@ function Queue () {
   }
 
   this.print = function () {
-    console.log(items.toString())
+    console.log(items)
   }
 }
 
-// var queue = new Queue()
-// console.log(queue.isEmpty())
-// queue.enqueue('John')
-// queue.enqueue('Jack')
-// queue.enqueue('Camila')
-// queue.print()
-// console.log(queue.size())
-// console.log(queue.isEmpty())
-// queue.dequeue()
-// queue.dequeue()
-// queue.print()
-
-// function PriorityQueue () {
-//   var items = []
-
-//   function QueueElement (element, priority) {
-//     this.element = element
-//     this.priority = priority
-//   }
-
-//   this.enqueue = function (element, priority) {
-//     var queueElement = new QueueElement(element, priority)
-//     if (this.isEmpty()) {
-//       items.push(queueElement)
-//     } else {
-//       var added = false
-//       for (var i = 0; i < items.length; i++) {
-//         if (queueElement.priority < items[i].priority) {
-//           items.splice(i, 0, queueElement)
-//           added = true
-//           break
-//         }
-//       }
-//       if (!added) {
-//         items.push(queueElement)
-//       }
-//     }
-//   }
-
-//   this.dequeue = function () {
-//     items.shift()
-//   }
-
-//   this.front = function () {
-//     return items[0]
-//   }
-
-//   this.isEmpty = function () {
-//     return items.length === 0
-//   }
-
-//   this.size = function () {
-//     return items.length
-//   }
-
-//   this.print = function () {
-//     console.log(items)
-//   }
-// }
-
+// 优先队列的使用
 // var priorityQueue = new PriorityQueue()
 
 // priorityQueue.enqueue('Jack', 2)
@@ -94,22 +105,29 @@ function Queue () {
 // priorityQueue.enqueue('James', 1)
 // priorityQueue.print()
 
+// 击鼓传花游戏
 function hotPotato (nameList, num) {
   var queue = new Queue()
 
+  // 参与者的名字入列
   for (var i = 0; i < nameList.length; i++) {
     queue.enqueue(nameList[i])
   }
-  // debugger
+
   var eliminated = ''
+
+  // 队列中的最后一个人为胜者
   while (queue.size() > 1) {
+    // 按设定的击鼓次数，每个人都从队列头部出列转到队列尾部（模拟传花）
     for (var i = 0; i < num; i++) {
       queue.enqueue(queue.dequeue())
     }
+    // 到了规定次数后，在队列头部的人（相当于拿到花）被淘汰
     eliminated = queue.dequeue()
     console.log(eliminated + '在击鼓传花游戏中被淘汰')
   }
 
+  // 胜者出列并被返回
   return queue.dequeue()
 }
 
--- stack.js
+++ stack.js
@@ -1,36 +1,45 @@
+// 用类来实现栈
 function Stack () {
   
   var items = []
 
+  // 栈顶添加
   this.push = function (element) {
     items.push(element)
   }
 
+  // 栈顶删除并返回删除元素
   this.pop = function () {
     return items.pop()
   }
 
+  // 返回栈顶元素
   this.peek = function () {
     return items[items.length - 1]
   }
 
+  // 栈是否为空
   this.isEmpty = function () {
     return items.length === 0
   }
 
+  // 返回栈里的元素个数
   this.size = function () {
     return items.length
   }
 
+  // 清空栈
   this.clear = function () {
     items = []
   }
 
+  // 打印栈
   this.print = function () {
     console.log(items.toString())
   }
 }
 
+// 一些对栈的操作
 // var stack = new Stack()
 // console.log(stack.isEmpty())
 // stack.push(5)
@@ -46,24 +55,46 @@ function Stack () {
 // console.log(stack.size())
 // stack.print()
 
-function divideBy2 (decNumber, base) {
+// 十进制转二进制
+function divideBy2 (decNumber) {
+  var remStack = new Stack(),
+      rem,
+      binaryString = ''
+  
+  while (decNumber > 0) {
+    rem = Math.floor(decNumber % 2)
+    remStack.push(rem)
+    decNumber = Math.floor(decNumber / 2)
+  }
+
+  while (!remStack.isEmpty()) {
+    binaryString += remStack.pop().toString()
+  }
+
+  return binaryString
+}
+
+// 把十进制转成任何进制
+function BaseConverter (decNumber, base) {
   var remStack = new Stack(),
       rem,
       binaryString = '',
       digits = '0123456789ABCDEF'
 
-
+  // 判断十进制数是否为0，把余数推入栈中
   while (decNumber > 0) {
     rem = Math.floor(decNumber % base)
     remStack.push(rem)
     decNumber = Math.floor(decNumber / base)
   }
 
+  // 把栈中的元素拼接打印出来
   while (!remStack.isEmpty()) {
     binaryString += digits[remStack.pop()]
   }
 
+  // 返回转换的二进制数
   return binaryString
 }
 
-console.log(divideBy2(233, 16))
+console.log(BaseConverter(233, 16))
